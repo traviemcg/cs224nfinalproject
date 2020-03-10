e5cf7d932e016c25741c12526fe440381b5adca1
Pranav Bhardwaj <pranavbhardwaj@Pranavs-MacBook-Pro.local>
2020-03-09 19:51:37 -0700

    question answering probes

---
+++ qa_probes.py
@@ -0,0 +1,158 @@
+import numpy as np
+from transformers import *
+import torch
+from tqdm import tqdm, trange
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from transformers.data.processors.squad import SquadV2Processor
+import sys
+
+
+
+def train_probes(model_prefix,
+                 data_dir,
+                 filename):
+    '''
+       Trains softmax probe corresponding to each layer of Albert
+
+    '''
+
+    tokenizer = AutoTokenizer.from_pretrained(model_prefix)
+    processor = SquadV2Processor()
+    examples = processor.get_train_examples(data_dir = data_dir, filename = filename)
+
+    features, dataset = squad_convert_examples_to_features(
+        examples=examples,
+        tokenizer=tokenizer,
+        max_seq_length=384,
+        doc_stride=128,
+        max_query_length=64,
+        is_training=False,
+        return_dataset="pt",
+        threads=1,
+    )
+
+    config = AlbertConfig.from_pretrained(model_prefix, output_hidden_states = True)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_prefix, config = config)
+
+    eval_sampler = SequentialSampler(dataset)
+    eval_dataloader = DataLoader(dataset, sampler = eval_sampler, batch_size = 4)
+
+    # multi-gpu evaluate
+    model = torch.nn.DataParallel(model)
+
+    for batch in tqdm(eval_dataloader, desc = "Evaluating"):
+        model.eval()
+        batch = tuple(t.to('cuda') for t in batch)
+        
+        with torch.no_grad():
+            inputs = {
+                    "input_ids": batch[0],
+                    "attention_mask": batch[1],
+                    "token_type_ids": batch[2],
+                }
+
+            # Albert forward pass
+            idx = batch[3]
+            outputs = model(**inputs)
+            attention_hidden_states = outputs[2][1:]
+
+            # Extract labels
+
+            # Update probes
+
+    # Save NN's
+
+
+def evaluate_probes(model_prefix,
+                    data_dir,
+                    filename):
+
+
+    tokenizer = AutoTokenizer.from_pretrained(model_prefix)
+    processor = SquadV2Processor()
+    examples = processor.get_train_examples(data_dir = data_dir, filename = filename)
+
+    features, dataset = squad_convert_examples_to_features(
+        examples=examples,
+        tokenizer=tokenizer,
+        max_seq_length=384,
+        doc_stride=128,
+        max_query_length=64,
+        is_training=False,
+        return_dataset="pt",
+        threads=1,
+    )
+
+    config = AlbertConfig.from_pretrained(model_prefix, output_hidden_states = True)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_prefix, config = config)
+
+    eval_sampler = SequentialSampler(dataset)
+    eval_dataloader = DataLoader(dataset, sampler = eval_sampler, batch_size = 4)
+
+    # multi-gpu evaluate
+    model = torch.nn.DataParallel(model)
+
+    for batch in tqdm(eval_dataloader, desc = "Evaluating"):
+        model.eval()
+        batch = tuple(t.to('cuda') for t in batch)
+        
+        with torch.no_grad():
+            inputs = {
+                    "input_ids": batch[0],
+                    "attention_mask": batch[1],
+                    "token_type_ids": batch[2],
+                }
+
+            # Albert forward pass
+            idx = batch[3]
+            outputs = model(**inputs)
+            attention_hidden_states = outputs[2][1:]
+
+            # Write predictions to file
+
+    
+
+
+
+if __name__ == "__main__":
+
+    # Train and dev set
+    train = "train-v2.0.json"
+    dev = "dev-v2.0.json"
+
+    # Model
+    if sys.argv[2] == "albert-base-v2":
+        model_prefix = "albert-base-v2"
+        output_prefix = "base-v2_" + sys.argv[1]
+
+    # Train softmax probes
+    train_probes(train)
+
+    # Evaluate softmax probes
+    evaluate_probes(dev)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

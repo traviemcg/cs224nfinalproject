17109d0a0274be927619ec9bf77b469c1cddc16f
Pranav Bhardwaj <pranavbhardwaj@Pranavs-MacBook-Pro.local>
2020-03-09 17:44:04 -0700

    bug search

--- qa_data_prep.py
+++ qa_data_prep.py
@@ -48,7 +48,7 @@ def extract_layers(model_prefix,
 
     for batch in tqdm(eval_dataloader, desc = "Evaluating"):
         model.eval()
-        batch = tuple(t.to('cuda:0') for t in batch)
+        batch = tuple(t.to('cuda') for t in batch)
         
         with torch.no_grad():
             inputs = {

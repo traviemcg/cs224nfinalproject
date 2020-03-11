3ec6cc2b86191993f756eb7751219cfc2eba4f10
Travis McGuire <travis.mcguire@stanford.edu>
2020-03-10 17:25:47 -0700

    Switch from possible to impossible in naming schema

--- SoftmaxRegression.py
+++ SoftmaxRegression.py
@@ -28,9 +28,9 @@ class IdxSoftmaxRegression(nn.Module):
         _, preds = p.max(-1)
         return preds
 
-class PossibleSoftmaxRegression(nn.Module):
+class ImpossibleSoftmaxRegression(nn.Module):
     def __init__(self, seq_len, hidden_size):
-        super(PossibleSoftmaxRegression, self).__init__()
+        super(ImpossibleSoftmaxRegression, self).__init__()
         self.seq_len = seq_len
         self.hidden_size = hidden_size
         self.W = nn.Linear(self.seq_len*self.hidden_size, 1, bias=True)
@@ -62,7 +62,7 @@ class MultiSoftmaxRegression():
         self.hidden_size = hidden_size
         self.model_start_idx = IdxSoftmaxRegression(seq_len, hidden_size)
         self.model_stop_idx = IdxSoftmaxRegression(seq_len, hidden_size)
-        self.model_is_possible = PossibleSoftmaxRegression(seq_len, hidden_size)
+        self.model_is_impossible = ImpossibleSoftmaxRegression(seq_len, hidden_size)
 
         self.lr = 1e-3
         self.max_grad_norm = 1.0
@@ -72,11 +72,11 @@ class MultiSoftmaxRegression():
             model = self.model_start_idx.to(device)
         elif mode == "stop":
             model = self.model_stop_idx.to(device)
-        elif mode == "possible":
-            model = self.model_is_possible.to(device)
+        elif mode == "impossible":
+            model = self.model_is_impossible.to(device)
         else:
             model = None
-            assert model != None, "Invalid value mode={}, should be 'start', 'stop', or 'possible'!".format(mode)
+            assert model != None, "Invalid value mode={}, should be 'start', 'stop', or 'impossible'!".format(mode)
 
         optimizer = torch.optim.Adam(model.parameters(), lr=float(self.lr))
             
@@ -90,16 +90,16 @@ class MultiSoftmaxRegression():
         return loss
 
     def predict(self, inputs, device):
-        # Return (batch_size, 2) array where both entries are -1 if is_ans==0
+        # Return (batch_size, 2) array where both entries are -1 if is_impossible==1
         with torch.no_grad():
             start_idx = self.model_start_idx.to(device).eval().predict(inputs)
             stop_idx = self.model_stop_idx.to(device).eval().predict(inputs)
-            is_possible = self.model_is_possible.to(device).eval().predict(inputs)
+            is_impossible = self.model_is_impossible.to(device).eval().predict(inputs)
 
             start_idx = start_idx+1 
             stop_idx = stop_idx+1
 
-            is_ans = 1-is_possible
+            is_ans = 1-is_impossible
             masked_start = start_idx*is_ans-1
             masked_stop = stop_idx*is_ans-1
 
@@ -110,10 +110,10 @@ class MultiSoftmaxRegression():
     def save(self, path):
         torch.save(self.model_start_idx.state_dict(), path+"_start_idx")
         torch.save(self.model_stop_idx.state_dict(), path+"_stop_idx")
-        torch.save(self.model_is_possible.state_dict(), path+"_is_possible")
+        torch.save(self.model_is_impossible.state_dict(), path+"_is_impossible")
 
     def load(self, path):
         self.model_start_idx.load_state_dict(torch.load(path+"_start_idx"))
         self.model_stop_idx.load_state_dict(torch.load(path+"_stop_idx"))
-        self.model_is_possible.load_state_dict(torch.load(path+"_is_possible"))
+        self.model_is_impossible.load_state_dict(torch.load(path+"_is_impossible"))
 

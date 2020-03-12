e77437f9e9095e779bc15c96537a6f7f91a4f3e3
Pranav Bhardwaj <pranavbhardwaj@DN0a2379cc.SUNet>
2020-03-11 21:53:55 -0700

    low data, many epoches

--- qa_probes.py
+++ qa_probes.py
@@ -31,7 +31,7 @@ def train_probes(model_prefix,
     processor = SquadV2Processor()
     examples = processor.get_train_examples(data_dir = data_dir, filename = filename)
 
-    # examples = examples[:8]
+    examples = examples[:8]
 
     # Extract features
     features, dataset = squad_convert_examples_to_features(
@@ -114,7 +114,7 @@ def evaluate_probes(model_prefix,
     processor = SquadV2Processor()
     examples = processor.get_train_examples(data_dir = data_dir, filename = filename)
 
-    examples = examples[:20]
+    examples = examples[:8]
 
     # Extract features
     features, dataset = squad_convert_examples_to_features(

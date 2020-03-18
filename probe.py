23fd80cef0d659cb7a6e1c174a941ff676c24c0c
Travis McGuire <travis.mcguire@stanford.edu>
2020-03-18 15:27:21 -0700

    Comments on predict, can be made to work over batches

--- probe.py
+++ probe.py
@@ -84,10 +84,12 @@ class Probe():
             inputs: tensor (batch_size, seq_len, hidden_size) are attention weighted hidden state outputs
             device: string ('cuda' or 'cpu') tells pytorch where to run computations
             threshold: float (e.g. 1.0 for no ans score*1.0) controlling tradeoff between answer and no answer prediction
-            context_start: integer (e.g. 1 if only start token at 0) can be specified to avoid selecting tokens in question
-            contenxt_end: integer (e.g. seq_len if full sequence) can be specified to avoid looking at tokens in padding
+            context_start: tensor (batch_size) can be specified to avoid selecting tokens in question e.g. 1 if only start token at 0
+            contenxt_end: tensor (batch_size) can be specified to avoid looking at tokens in padding e.g. seq_len if full sequence
             max_answer_length: integer (e.g. 17 for longest in Squad2.0) constraining search space by giving maximum answer length
         """
+
+        # TODO: Make work over batches of greater than size 1
 
         _, seq_len, _ = inputs.shape
         if context_end == None:
